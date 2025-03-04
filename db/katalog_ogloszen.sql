756e25a6aac2826055abcbb16bdae42047691109
Lukkolod <lukkolod@gmail.com>
2025-03-04 20:15:46 +0100

    aktualizacja bazy danych

--- db/katalog_ogloszen.sql
+++ db/katalog_ogloszen.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 03, 2025 at 03:22 PM
+-- Generation Time: Mar 04, 2025 at 08:14 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -33,8 +33,23 @@ CREATE TABLE `uzytkownicy` (
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `verification_token` varchar(255) DEFAULT NULL,
-  `active` tinyint(1) DEFAULT 0
+  `active` tinyint(1) DEFAULT 0,
+  `imie` varchar(100) DEFAULT NULL,
+  `nazwisko` varchar(100) DEFAULT NULL,
+  `adres` varchar(255) DEFAULT NULL,
+  `nr_domu` varchar(10) DEFAULT NULL,
+  `nr_lokalu` varchar(10) DEFAULT NULL,
+  `kod_pocztowy` varchar(10) DEFAULT NULL,
+  `miejscowosc` varchar(100) DEFAULT NULL,
+  `nip` varchar(12) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id`, `username`, `email`, `password`, `verification_token`, `active`, `imie`, `nazwisko`, `adres`, `nr_domu`, `nr_lokalu`, `kod_pocztowy`, `miejscowosc`, `nip`) VALUES
+(24, 'marek', 'lukkolod@gmail.com', '$2y$10$Hwwe8Zu7lT2WNOewg5ljY.b96L4s..NX7sE6crKDp3O.TrDWOU5QC', 'fe2ae630f41e52b49496543c33604217611c99acdfd0c8f1f772cc1709c72ca8be7f31a8101bfcd79f58867ab2b2f524008f', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -56,7 +71,7 @@ ALTER TABLE `uzytkownicy`
 -- AUTO_INCREMENT for table `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
