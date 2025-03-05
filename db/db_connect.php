d3a5b1df55bb61b56a6aaf4d0a6786edbf438535
Lukkolod <lukkolod@gmail.com>
2025-03-05 13:11:12 +0100

    baza danych na serwerze

--- db/db_connect.php
+++ db/db_connect.php
@@ -1,9 +1,16 @@
 <?php
 
-$server = 'localhost';
-$user = 'root';
-$password = '';
-$db = 'katalog_ogloszen';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../'); 
+$dotenv->load();
+
+$db_password = $_ENV['DB_PASSWORD'] ?? null;
+
+$server = 'sql313.infinityfree.com';
+$user = 'if0_38451400';
+$password = $db_password;
+$db = 'if0_38451400_katalog_ogloszen';
 
 $conn = mysqli_connect($server, $user, $password, $db) or die("Błąd połączenia: " . mysqli_connect_error());
 
